a4aca58af2dc1b34c3a9f7cb062bcacfb0daaa10
binathlaknaka <kablaknaka@students.nsbm.ac.lk>
2024-11-28 11:46:06 +0530

    commit 1

---
+++ leaderboard/src/App.js
@@ -0,0 +1,14 @@
+import Board from './component/borad';
+import './component/style.css';
+
+function App() {
+  return (
+    <div className="App" id='main'>
+      <Board>
+
+      </Board>
+    </div>
+  );
+}
+
+export default App;
